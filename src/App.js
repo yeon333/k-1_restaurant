919ae8d340f4536a55e0968174a0cc864cd82ef7
Sungmin Park <holypp@naver.com>
2022-04-11 15:44:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+//import './Map';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="orange-nav">
+        <div>K-1 슐랭</div>
+      </div>
+      <span className="title" >K-1 chelin</span>
+      <div class="search-container">
+        <input class="search-bar" type="text" placeholder="식당/지역" />
+        <button id="search">검색</button>
+      </div>
     </div>
   );
 }
